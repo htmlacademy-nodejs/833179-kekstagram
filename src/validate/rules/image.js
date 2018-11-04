8c517fea56cf09a025f10c678ed2012e7bb9ccb4
Dmytro Zuienko <dimazuien@gmail.com>
2018-11-04 16:11:04 +0200

    Send errors as JSON

--- src/validate/rules/image.js
+++ src/validate/rules/image.js
@@ -5,7 +5,7 @@ const fileType = require(`file-type`);
 const imageMimeTypes = [`image/jpg`, `image/png`, `image/jpeg`];
 
 const path = {
-  validator: (field) => imageMimeTypes.includes(fileType(field).mime),
+  validator: (field) => imageMimeTypes.includes(fileType(field) && fileType(field).mime),
   message: (field) => `Value of the field ${field} must be an image`
 };
 
